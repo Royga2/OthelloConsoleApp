f0258939c2638fa9de886f5309f1b234e5829164
Royga2 <galiliroy@gmail.com>
2023-02-23 19:39:33 +0200

    Fixed Modifier

--- Player.cs
+++ Player.cs
@@ -5,24 +5,23 @@ using System.Text;
 
 namespace Othello
 {
-    public enum eColor
+    public class Player
     {
-        None = 0,
-        Black,
-        White
+        public enum eColor
+        {
+            None = 0,
+            Black,
+            White
+        }
 
-    }
-
-    class Player
-    {
         private readonly string r_PlayerName;
         private readonly eColor r_PlayerColor;
         private readonly bool r_IsComputer;
 
 
-        public Player()
+        public Player(string i_Name)
         {
-            r_PlayerName = setPlayerName();
+            r_PlayerName = i_Name;
             r_PlayerColor = eColor.Black;
             r_IsComputer = false;
         }
@@ -35,49 +34,27 @@ namespace Othello
             r_IsComputer = false;
         }
        
-        public Player(eColor i_OponentColor, bool i_IsComputer)
+        public Player(string i_Name, eColor i_OponentColor, bool i_IsComputer)
         {
 
-            r_PlayerName = i_IsComputer == true ? "CPU" : setPlayerName();
+            r_PlayerName = i_IsComputer == true ? "CPU" : i_Name;
             r_PlayerColor = i_OponentColor == eColor.Black ? eColor.White : eColor.Black;
             r_IsComputer = i_IsComputer;
         }
         
         public bool IsComputer
         {
-            get{return r_IsComputer; }
+            get {return r_IsComputer; }
         }
         
         public eColor PlayerColor
         {
-            get
-            {
-                return r_PlayerColor;
-            }
+            get { return r_PlayerColor; }
         }
         
         public string PlayerName
         {
-            get
-            {
-                return r_PlayerName;
-            }
-        }
-        
-        private string setPlayerName()
-        {
-
-            Console.WriteLine("Please enter your name: ");
-            string playerName = Console.ReadLine();
-
-            while (playerName.Length <= 0 || playerName.Length > 30)
-            {
-                Console.WriteLine(@"Error: name must be at list one characters and no longer than 30 characters,
-Please try again");
-                playerName = Console.ReadLine();
-            }
-            Console.Clear();
-            return playerName;
+            get { return r_PlayerName; }
         }
     }
 }
